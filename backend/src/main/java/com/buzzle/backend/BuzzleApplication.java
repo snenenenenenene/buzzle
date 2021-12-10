6eaf22be8267462c5ea47513aa56163359175612
snenenenenenene <sennebels@gmail.com>
2021-12-10 13:25:49 +0100

    Actually added maven and stuff

---
+++ backend/src/main/java/com/buzzle/backend/BuzzleApplication.java
@@ -0,0 +1,13 @@
+package com.buzzle.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzzleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuzzleApplication.class, args);
+    }
+
+}
